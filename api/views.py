dbbaa0a0335cfaf3eda39c0c9dd28bee0d3b401a
Siddhant Manglam <siddhantmanglam@Siddhants-MacBook-Air.local>
2025-06-24 15:07:36 +0530

    Added api setup and seed data integration

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .serializers import UserSerializer, ProfileSerializer, DuelMatchSerializer, StreakSerializer
-from users.models import CustomUser, Profile, DuelMatch, Streak
+from .serializers import UserSerializer, ProfileSerializer, DuelMatchSerializer, StreakSerializer , NewsArticleSerializer , DailyNewsReadSerializer, MessageSerializer
+from users.models import CustomUser, Profile, DuelMatch, Streak , NewsArticle , DailyNewsRead , Message
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,5 +18,17 @@ class DuelMatchViewSet(viewsets.ModelViewSet):
 class StreakViewSet(viewsets.ModelViewSet):
     queryset = Streak.objects.all()
     serializer_class = StreakSerializer
+
+class NewsArticleViewSet(viewsets.ModelViewSet):
+    queryset = NewsArticle.objects.all().order_by('-published_at')
+    serializer_class = NewsArticleSerializer
+
+class DailyNewsReadViewSet(viewsets.ModelViewSet):
+    queryset = DailyNewsRead.objects.all().order_by('-read_date')
+    serializer_class = DailyNewsReadSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().order_by('-timestamp')
+    serializer_class = MessageSerializer
 
 
